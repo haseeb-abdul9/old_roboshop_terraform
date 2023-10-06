6da6f730254c25360646fc719c810e1bc1a20f88
mohammad9haseeb <mohammad9haseeb@gmail.com>
2023-10-06 09:10:38 -0400

    for each for server & DNS

--- servers.tf
+++ servers.tf
@@ -8,6 +8,10 @@ data "aws_security_group" "allow-all" {
   name = "allow all"
 }
 
+variable "instance_type" {
+  default = "t3.small"
+}
+
 variable "components" {
   default = {
     frontend = {
@@ -22,7 +26,7 @@ variable "components" {
 
     catalogue = {
       name = "catalogue"
-      instance_type = "t3.small"
+      instance_type = "t3.micro"
     }
 
     redis = {
@@ -32,12 +36,12 @@ variable "components" {
 
     user = {
       name = "user"
-      instance_type = "t3.small"
+      instance_type = "t3.micro"
     }
 
     cart = {
       name = "cart"
-      instance_type = "t3.small"
+      instance_type = "t3.micro"
     }
 
     mysql = {
@@ -47,7 +51,7 @@ variable "components" {
 
     shipping = {
       name = "shipping"
-      instance_type = "t3.small"
+      instance_type = "t3.medium"
     }
 
     rabbitmq = {
@@ -72,4 +76,195 @@ resource "aws_instance" "instance" {
     Name = each.value["name"]
   }
 }
+
+resource "aws_route53_record" "records" {
+  for_each = var.components
+  zone_id = "Z07904683H2P61IIEYSB9"
+  name    = "${each.value["name"]}-dev.haseebdevops.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.instance[each.value["name"]].private_ip]
+}
+
+/*
+resource "aws_instance" "frontend" {
+  ami           = data.aws_ami.centos.image_id
+  instance_type = var.instance_type
+  vpc_security_group_ids = [data.aws_security_group.allow-all.id]
+  tags = {
+    Name = "frontend"
+  }
+}
+*/
+
+/*resource "aws_route53_record" "frontend" {
+  zone_id = "Z07904683H2P61IIEYSB9"
+  name    = "frontend-dev.haseebdevops.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.frontend.private_ip]
+}
+
+output "frontend" {
+  value = aws_instance.frontend.public_ip
+}
+
+resource "aws_instance" "mongodb" {
+  ami           = data.aws_ami.centos.image_id
+  instance_type = var.instance_type
+  vpc_security_group_ids = [data.aws_security_group.allow-all.id]
+  tags = {
+    Name = "mongodb"
+  }
+}
+
+resource "aws_route53_record" "mongodb" {
+  zone_id = "Z07904683H2P61IIEYSB9"
+  name    = "mongodb-dev.haseebdevops.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.mongodb.private_ip]
+}
+
+resource "aws_instance" "catalogue" {
+  ami           = data.aws_ami.centos.image_id
+  instance_type = var.instance_type
+  vpc_security_group_ids = [data.aws_security_group.allow-all.id]
+  tags = {
+    Name = "catalogue"
+  }
+}
+
+resource "aws_route53_record" "catalogue" {
+  zone_id = "Z07904683H2P61IIEYSB9"
+  name    = "catalogue-dev.haseebdevops.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.catalogue.private_ip]
+}
+
+
+resource "aws_instance" "redis" {
+  ami           = data.aws_ami.centos.image_id
+  instance_type = var.instance_type
+  vpc_security_group_ids = [data.aws_security_group.allow-all.id]
+  tags = {
+    Name = "redis"
+  }
+}
+
+resource "aws_route53_record" "redis" {
+  zone_id = "Z07904683H2P61IIEYSB9"
+  name    = "redis-dev.haseebdevops.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.redis.private_ip]
+}
+
+
+resource "aws_instance" "user" {
+  ami           = data.aws_ami.centos.image_id
+  instance_type = var.instance_type
+  vpc_security_group_ids = [data.aws_security_group.allow-all.id]
+  tags = {
+    Name = "user"
+  }
+}
+
+resource "aws_route53_record" "user" {
+  zone_id = "Z07904683H2P61IIEYSB9"
+  name    = "user-dev.haseebdevops.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.user.private_ip]
+}
+
+
+resource "aws_instance" "cart" {
+  ami           = data.aws_ami.centos.image_id
+  instance_type = var.instance_type
+  vpc_security_group_ids = [data.aws_security_group.allow-all.id]
+  tags = {
+    Name = "cart"
+  }
+}
+
+resource "aws_route53_record" "cart" {
+  zone_id = "Z07904683H2P61IIEYSB9"
+  name    = "cart-dev.haseebdevops.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.cart.private_ip]
+}
+
+
+resource "aws_instance" "mysql" {
+  ami           = data.aws_ami.centos.image_id
+  instance_type = var.instance_type
+  vpc_security_group_ids = [data.aws_security_group.allow-all.id]
+  tags = {
+    Name = "mysql"
+  }
+}
+
+resource "aws_route53_record" "mysql" {
+  zone_id = "Z07904683H2P61IIEYSB9"
+  name    = "mysql-dev.haseebdevops.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.mysql.private_ip]
+}
+
+resource "aws_instance" "shipping" {
+  ami           = data.aws_ami.centos.image_id
+  instance_type = var.instance_type
+  vpc_security_group_ids = [data.aws_security_group.allow-all.id]
+  tags = {
+    Name = "shipping"
+  }
+}
+
+resource "aws_route53_record" "shipping" {
+  zone_id = "Z07904683H2P61IIEYSB9"
+  name    = "shipping-dev.haseebdevops.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.shipping.private_ip]
+}
+
+
+resource "aws_instance" "rabbitmq" {
+  ami           = data.aws_ami.centos.image_id
+  instance_type = var.instance_type
+  vpc_security_group_ids = [data.aws_security_group.allow-all.id]
+  tags = {
+    Name = "rabbitmq"
+  }
+}
+
+resource "aws_route53_record" "rabbitmq" {
+  zone_id = "Z07904683H2P61IIEYSB9"
+  name    = "rabbitmq-dev.haseebdevops.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.rabbitmq.private_ip]
+}
+
+
+resource "aws_instance" "payment" {
+  ami           = data.aws_ami.centos.image_id
+  instance_type = var.instance_type
+  vpc_security_group_ids = [data.aws_security_group.allow-all.id]
+  tags = {
+    Name = "payment"
+  }
+}
+
+resource "aws_route53_record" "payment" {
+  zone_id = "Z07904683H2P61IIEYSB9"
+  name    = "payment-dev.haseebdevops.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.payment.private_ip]
+}*/
 
